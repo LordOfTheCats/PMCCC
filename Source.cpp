b6d386d5df7b8ef12d71b0bebffdab446aada59c
LordOfTheCats <robbolob.bobbowob@gmail.com>
2017-02-23 17:37:27 +0000

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <math.h>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+struct node {
+	float data;
+	node* next;
+
+	void append(float x) {
+		if (next == NULL) {
+			node* temp;
+			temp = (node*)malloc(sizeof(node));
+			temp->data = x;
+			temp->next = NULL;
+			next = temp;
+		} else {
+			next->append(x);
+		}
+	}
+
+	float retrieve(int p) {
+		node* anotherOne = this;
+		for (int i = 0; i < p; i++) {
+			if (anotherOne->next != NULL) {
+				anotherOne = anotherOne->next;
+			} else {
+				throw "Shit";
+			}
+		}
+		return anotherOne->data;
+	}
+
+	node() {
+		data = 0;
+		next = NULL;
+	};
+};
+
+int main(void) {
+	ifstream file("Text.txt");
+	string line;
+	while (getline(file, line)) {
+		cout << line<<"\n";
+	}
+
+	float n, temp, Sxy, Sxx, Syy, Sx, Sy, Sxs, Sys, Sexy, r;
+	
+	cout << "How many sets of values?\n>";
+	cin >> n;
+
+	node lillyX, lillyY;
+
+	cout << "Please input each x value one at a time.\n>";
+	cin >> lillyX.data;
+
+	for (int i = 1; i < n; i++) {
+		cout << ">";
+		cin >> temp;
+		lillyX.append(temp);
+	}
+
+	cout << "Please input each y value one at a time.\n>";
+	cin >> lillyY.data;
+
+	for (int i = 1; i < n; i++) {
+		cout << ">";
+		cin >> temp;
+		lillyY.append(temp);
+	}
+
+	Sx = 0;
+	Sy = 0;
+	Sxs = 0;
+	Sys = 0;
+	Sexy = 0;
+	Sxy = 0;
+	for (int i = 0; i < n; i++) {
+		Sx += lillyX.retrieve(i);
+		Sy += lillyY.retrieve(i);
+		Sxs += pow(lillyX.retrieve(i),2);
+		Sys += pow(lillyY.retrieve(i),2);
+		Sexy += lillyX.retrieve(i) * lillyY.retrieve(i);
+	}
+	cout << "Sx:\t" << Sx << "\n";
+	cout << "Sy:\t" << Sy << "\n";
+	cout << "Sxs:\t" << Sxs << "\n";
+	cout << "Sys:\t" << Sys << "\n";
+
+	Sxx = Sxs - (pow(Sx, 2) / n);
+	cout << "Sxx:\t" << Sxx << "\n";
+	Syy = Sys - (pow(Sy, 2) / n);
+	cout << "Syy:\t" << Syy << "\n";
+	Sxy = Sexy - ((Sx * Sy) / n);
+	cout << "Sxy:\t" << Sxy << "\n";
+
+	r = Sxy / (sqrt(Sxx * Syy));
+
+	cout << "\nPMCC:\t" << r;
+	cin >> temp;
+}
